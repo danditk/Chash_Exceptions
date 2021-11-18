67ae6d10fb803cd671376f0089a668474f432e79
Darek <darekt@abmicro.pl>
2021-11-18 20:15:54 +0100

    Division exception try catch and throw exception

--- Program.cs
+++ Program.cs
@@ -10,11 +10,28 @@ namespace Chash_Exceptions
     {
         static void Main(string[] args)
         {
-
+            try
+            {
+                Console.WriteLine("Wynik: " + Division(0));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Nie nie da się podzielić przez zero, a Ty... " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("YYY, coś poszło nie tak!" + e.Message);
+            }
+            Console.ReadKey();
         }
 
-        float Division(int a)
+        public static float Division(int a)
         {
+            if (a == 5)
+            {
+                throw new Exception("Nigdy nie dziel przez 5!");
+            }
+
             return 10 / a;
         }
     }
